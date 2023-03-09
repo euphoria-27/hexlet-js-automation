099cb00df03b63de3b357f835dd26f79f2b52e3c
kartemen <kostikkms@gmail.com>
2023-03-09 22:50:18 +0300

    refactor capitalzie test with asserts

--- tests/capitalize.test.js
+++ tests/capitalize.test.js
@@ -1,11 +1,15 @@
+import { strict as assert} from 'node:assert';
 import { capitalize } from "../src/capitalize.js"
 
-if(capitalize('hello') !== 'Hello') {
-    throw new Error('function not working')
-}
+// if(capitalize('hello') !== 'Hello') {
+//     throw new Error('function not working')
+// }
 
-if(capitalize('') !== '') {
-    throw new Error('function not working')
-}
+// if(capitalize('') !== '') {
+//     throw new Error('function not working')
+// }
+
+assert.equal(capitalize(''), '')
+assert.equal(capitalize('hello'), 'Hello')
 
 console.log('All tests passed')
